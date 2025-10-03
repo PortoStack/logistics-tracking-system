42e01a3f38c41ce5bb1a6a28c92e0026591b1cb6
porto <yospunyaporto@gmail.com>
2025-10-03 16:49:40 +0700

    Add project files.

---
+++ LogisticsTrackingSystem/dataContract.cs
@@ -0,0 +1,22 @@
+﻿using LogisticsTrackingSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogisticsTrackingSystem
+{
+    public class CustomerObj
+    {
+        public int id;
+        public string name;
+        public string email;
+        public string phone;
+        public string type; 
+    }
+
+    public class RouteObj
+    {
+
+    }
+}
